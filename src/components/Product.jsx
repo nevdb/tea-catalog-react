232d42473620a20d772e1935219f824ffe49933f
nevena <dnevena27@gmail.com>
2026-02-09 11:32:40 +0200

    Linking the context to state

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,10 @@
-export default function Product({
-  id,
-  image,
-  title,
-  price,
-  description,
-  onAddToCart,
-}) {
+import { useContext } from "react";
+
+import { CartContext } from "./store/shopping-card-context";
+
+export default function Product({ id, image, title, price, description }) {
+  const { addItemToCart } = useContext(CartContext);
+
   return (
     <article className="flex flex-col gap-4 rounded-lg p-4 bg-amber-50 shadow-md">
       <img
@@ -21,7 +20,7 @@ export default function Product({
       </div>
       <p className="text-right">
         <button
-          onClick={() => onAddToCart(id)}
+          onClick={() => addItemToCart(id)}
           className="w- bg-lime-800 hover:bg-lime-600 text-lime-100 border-0 rounded-md py-2 px-4 text-base cursor-pointer"
         >
           Add to Cart
